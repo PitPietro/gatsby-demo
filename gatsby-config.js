65509332730d7fe478d95d031ca44bd5c3b723df
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-12-24 10:47:12 +0100

    part 4 - data in gatsby rev2

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: `Pietro Poluzzi`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-emotion`,
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,23 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { css } from "@emotion/react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
-export default function About() {
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+export default function About({ data }) {
     return (
       <Layout>
         <SEO title="About" />
-          <h1>About me</h1>
+        <h1>About {data.site.siteMetadata.title}</h1>
           <p>
               I’m good enough, I’m smart enough, and gosh darn it, people like me!
           </p>
