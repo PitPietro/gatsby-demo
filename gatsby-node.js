b69002f93446eaf6895762e9f54f8abb1f18404f
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-12-25 23:53:34 +0100

    part 7 - Gatsby data layer

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,55 @@
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
+ * To implement an API, just export a function with the name of the API.
+ *
+ * onCreateNode function will be called by Gatsby whenever a new node is created (or updated).
+ * createNodeField allows you to create additional fields on nodes created by other plugins.
+ * Docs: https://www.gatsbyjs.com/docs/tutorial/part-seven/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async  ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const { createPage } = actions
+  const result = await graphql(`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
